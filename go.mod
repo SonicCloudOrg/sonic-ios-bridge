a8abae4c00af5eb019c04257b0d294cc25c520fc
ZhouYixun <291028775@qq.com>
2022-02-19 22:11:07 +0800

    get_source_demo

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/SonicCloudOrg/sonic-ios-bridge
 go 1.18
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/spf13/cobra v1.3.0
 	howett.net/plist v1.0.0
 )
 
 require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/opesun/goquery v0.0.0-20160908163916-0d77e43213cd // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 )
--- main.go
+++ main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	resp, _ := http.Get("https://www.theiphonewiki.com/wiki/Models")
-	defer resp.Body.Close()  // 函数结束时关闭Body
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	var url = "https://www.theiphonewiki.com/wiki/Models"
+	p, _ := goquery.NewDocument(url)
+
+	pTitle := p.Find(".wikitable").Eq(0) //直接提取title的内容
+
+	th := pTitle.Find("th")
+	var gIndex = 0
+	var Iindex = 0
+	th.Map(func(i int, s *goquery.Selection) string {
+		if s.Text() == "Generation\n" {
+			gIndex = i
+		}
+		if s.Text() == "Identifier\n" {
+			Iindex = i
+		}
+		return s.Text()
+	})
+	tr:=pTitle.Find("td")
+	fmt.Println(tr.Eq(gIndex).Text())
+	fmt.Println(tr.Eq(Iindex).Text())
+	//	if t.Data=="Generation" {
+	//		fmt.Println(i)
+	//	}
+	//	if t.Data=="Identifier"{
+	//		fmt.Println(i)
+	//	}
+	//}
+	//o:=i.Find("td").Eq(0).Text()
+	//fmt.Println(o)
+
 	//cmd.Execute()
 }
