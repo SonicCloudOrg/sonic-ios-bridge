16be07b6a3b08531f239c66d270462508fd50f5a
ZhouYixun <291028775@qq.com>
2022-03-02 23:59:01 +0800

    done

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Version code of sib",
+	Long:  "Version code of sib",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v1.0.1")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
