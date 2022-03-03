8c70cf4710ecc8168608d2ec18d814a6e3352a38
ZhouYixun <291028775@qq.com>
2022-03-04 00:42:23 +0800

    init_uninstall

---
+++ cmd/uninstall.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/SonicCloudOrg/sonic-ios-bridge/src/util"
+	giDevice "github.com/electricbubble/gidevice"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var uninstallCmd = &cobra.Command{
+	Use:   "uninstall",
+	Short: "Uninstall App in your device",
+	Long:  "Uninstall App in your device",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		usbMuxClient, err := giDevice.NewUsbmux()
+		if err != nil {
+			return util.NewErrorPrint(util.ErrConnect, "usbMux", err)
+		}
+		list, err1 := usbMuxClient.Devices()
+		if err1 != nil {
+			return util.NewErrorPrint(util.ErrSendCommand, "listDevices", err1)
+		}
+		if len(list) != 0 {
+			var device giDevice.Device
+			if len(udid) != 0 {
+				for i, d := range list {
+					if d.Properties().SerialNumber == udid {
+						device = list[i]
+						break
+					}
+				}
+			} else {
+				device = list[0]
+			}
+			if device.Properties().SerialNumber != "" {
+				errInstall := device.AppInstall(path)
+				if errInstall != nil {
+					fmt.Println("uninstall failed")
+					os.Exit(0)
+				}
+			} else {
+				fmt.Println("device no found")
+				os.Exit(0)
+			}
+		} else {
+			fmt.Println("no device connected")
+			os.Exit(0)
+		}
+		fmt.Println("uninstall successful")
+		return nil
+	},
+}
+
+var bundleId string
+
+func init() {
+	appCmd.AddCommand(uninstallCmd)
+	uninstallCmd.Flags().StringVarP(&udid, "udid", "u", "", "device's serialNumber")
+	uninstallCmd.Flags().StringVarP(&bundleId, "bundleId", "b", "", "target bundleId")
+	uninstallCmd.MarkFlagRequired("bundleId")
+}
