d2c17952be92dbf52df0f1bbc74c833d4f588c12
sam80180 <sam80180@gmail.com>
2024-01-07 01:45:19 +0800

    fix: fix 'no more responses from usbmuxd after the last device disconnects' problem

--- go.mod
+++ go.mod
@@ -6,12 +6,14 @@ require (
 	github.com/Masterminds/semver v1.5.0
 	github.com/SonicCloudOrg/sonic-gidevice v0.7.7
 	github.com/SonicCloudOrg/sonic-ios-webkit-adapter v0.0.7
+	github.com/cenkalti/backoff/v4 v4.2.1
 	github.com/gin-gonic/gin v1.8.1
 	github.com/google/gopacket v1.1.19
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/hashicorp/go-envparse v0.1.0
 	github.com/mitchellh/mapstructure v1.5.0
+	github.com/quintans/toolkit v0.3.5
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.4.0
@@ -32,7 +34,7 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lunixbochs/struc v0.0.0-20200707160740-784aaebc1d40 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -43,10 +45,10 @@ require (
 	github.com/tidwall/sjson v1.2.5 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/yezihack/e v1.0.0 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
+	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
 	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- src/util/usbmux.go
+++ src/util/usbmux.go
@@ -1,51 +1,148 @@
 package util
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"os"
 	"os/signal"
+	"syscall"
+	"time"
 
 	giDevice "github.com/SonicCloudOrg/sonic-gidevice"
+	"github.com/SonicCloudOrg/sonic-gidevice/pkg/libimobiledevice"
 	"github.com/SonicCloudOrg/sonic-ios-bridge/src/entity"
+	"github.com/cenkalti/backoff/v4"
+	"github.com/quintans/toolkit/latch"
+	"github.com/sirupsen/logrus"
 )
 
-func UsbmuxListen(cb func(gidevice *giDevice.Device, device *entity.Device, e error, cancelFunc func())) (func(), error) {
-	usbMuxClient, err := giDevice.NewUsbmux()
-	if err != nil {
-		return nil, NewErrorPrint(ErrConnect, "usbMux", err)
-	}
+func UsbmuxListen(cbOnData func(gidevice *giDevice.Device, device *entity.Device, e error, cancelFunc context.CancelFunc)) context.CancelFunc {
 	usbmuxInput := make(chan giDevice.Device)
-	shutdownUsbmuxFun, err2 := usbMuxClient.Listen(usbmuxInput)
-	if err2 != nil {
-		return nil, NewErrorPrint(ErrSendCommand, "listen", err2)
-	}
-	shutDown := make(chan os.Signal, 1)
-	signal.Notify(shutDown, os.Interrupt, os.Kill)
-	go func() {
+	sigTerm := make(chan os.Signal, 1)
+	signal.Notify(sigTerm, os.Interrupt, os.Kill)
+	var funcCancelListen context.CancelFunc
+	healthCheck := make(chan bool)
+	mylatch := latch.NewCountDownLatch()
+	mylatch.Add(2)
+	go (func() {
+		backoffAlgorithm := backoff.NewExponentialBackOff()
+		bIsOk := true
+		backoff.RetryNotify(func() error {
+			if mylatch.Counter() <= 0 {
+				return nil
+			}
+			usbMuxClient, err := giDevice.NewUsbmux()
+			if err != nil {
+				return NewErrorPrint(ErrConnect, "usbMux", err)
+			}
+			for {
+				if mylatch.Counter() <= 0 {
+					return nil
+				}
+				time.Sleep(5 * time.Second)
+				if _, errBuid := usbMuxClient.ReadBUID(); errBuid != nil {
+					return errBuid
+				}
+				logrus.Debug("usbmux health check success")
+				if !bIsOk { // transition from not OK to OK
+					logrus.Trace("Reset health check backoff algorithm")
+					backoffAlgorithm.Reset()
+					bIsOk = true
+				}
+			}
+		}, backoffAlgorithm, func(err error, d time.Duration) {
+			bIsOk = false
+			logrus.Warnf("usbmux health check error: %+v", err)
+			healthCheck <- false
+		})
+		logrus.Trace("end health check")
+	})()
+	go (func(funcStop *context.CancelFunc) {
+		backoffAlgorithm := backoff.NewExponentialBackOff()
+		bIsOk := true
+		backoff.RetryNotify(func() error {
+			if mylatch.Counter() <= 1 { // 'read channel input' go routine is stopped
+				return nil
+			}
+			usbMuxClient, err := giDevice.NewUsbmux()
+			if err != nil {
+				return NewErrorPrint(ErrConnect, "usbMux", err)
+			}
+			shutDownFun, errListen := usbMuxClient.Listen(usbmuxInput)
+			(*funcStop) = shutDownFun
+			if errListen != nil {
+				return NewErrorPrint(ErrSendCommand, string(libimobiledevice.MessageTypeListen), errListen)
+			}
+			logrus.Info("Start listening...")
+			<-healthCheck // empty out the channel
+			if !bIsOk {   // transition from not OK to OK
+				logrus.Trace("Reset usbmux listen backoff algorithm")
+				backoffAlgorithm.Reset()
+				bIsOk = true
+			}
+			for range healthCheck {
+				logrus.Info("Cancel listening")
+				(*funcStop) = nil
+				return fmt.Errorf("usbmux listening is cancelled")
+			}
+			return nil
+		}, backoffAlgorithm, func(err error, d time.Duration) {
+			bIsOk = false
+			logrus.Warnf("usbmux listening error: %+v", err)
+		})
+		mylatch.Done()
+		logrus.Trace("end usbmux listen")
+	})(&funcCancelListen)
+	go (func(funcStop *context.CancelFunc) {
+		numOnlineDevices := 0
+	loopRead:
 		for {
 			select {
 			case d, ok := <-usbmuxInput:
-				if !ok { // usbmux channel closed
-					close(shutDown)
-					return
+				if !ok { // channel is closed
+					logrus.Info("usbmux input channel is closed")
+					break loopRead
 				}
 				if d == nil {
 					continue
 				}
 				deviceByte, _ := json.Marshal(d.Properties())
-				device := &entity.Device{}
-				errDecode := json.Unmarshal(deviceByte, device)
-				if errDecode != nil {
-					cb(nil, nil, errDecode, shutdownUsbmuxFun)
-					continue
+				var device entity.Device
+				errDec := json.Unmarshal(deviceByte, &device)
+				var ptrEntityDevice *entity.Device = &device
+				if errDec == nil {
+					device.Status = device.GetStatus()
+					if device.Status == "online" {
+						numOnlineDevices += 1
+					} else {
+						numOnlineDevices -= 1
+					}
+				} else {
+					ptrEntityDevice = nil
 				}
-				device.Status = device.GetStatus()
-				cb(&d, device, nil, shutdownUsbmuxFun)
-			case <-shutDown:
-				shutdownUsbmuxFun()
-				return
+				var _fStop context.CancelFunc
+				if funcStop != nil {
+					_fStop = *funcStop
+				}
+				if cbOnData != nil {
+					cbOnData(&d, ptrEntityDevice, errDec, _fStop)
+				}
+				logrus.Debugf("Number of online devices= %d", numOnlineDevices)
+				if numOnlineDevices <= 0 {
+					logrus.Info("No devices are online")
+				}
+			case <-sigTerm:
+				logrus.Info("Stop listening")
+				if funcStop != nil {
+					(*funcStop)()
+				}
+				os.Exit(128 + int(syscall.SIGTERM)) // https://itsfoss.com/linux-exit-codes/#code-143-or-sigterm
+				break loopRead
 			}
 		}
-	}()
-	return shutdownUsbmuxFun, nil
+		mylatch.Done()
+		logrus.Trace("end reading channel input")
+	})(&funcCancelListen)
+	return funcCancelListen
 }
