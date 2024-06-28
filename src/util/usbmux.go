b23aef3b9e49b33ffdca32c61b4dbc0e14390419
sam80180 <sam80180@gmail.com>
2024-06-29 02:50:53 +0800

    fix: close usbmux client when not used

--- src/util/usbmux.go
+++ src/util/usbmux.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"reflect"
 	"syscall"
 	"time"
+	"unsafe"
 
 	giDevice "github.com/SonicCloudOrg/sonic-gidevice"
 	"github.com/SonicCloudOrg/sonic-ios-bridge/src/entity"
@@ -17,6 +19,20 @@ import (
 )
 
 type UsbmuxListenCallback func(gidevice *giDevice.Device, device *entity.Device, e error, cancelFunc context.CancelFunc)
+
+func CloseUsbmuxClient(usbMuxClient giDevice.Usbmux) error {
+	var output_err error = nil
+	TryCatch{}.Try(func() {
+		// https://stackoverflow.com/a/59196685/12857692
+		v := reflect.ValueOf(usbMuxClient).Elem()
+		f := v.FieldByName("client")
+		rf := reflect.NewAt(f.Type(), unsafe.Pointer(f.UnsafeAddr())).Elem()
+		rf.MethodByName("Close").Call(nil)
+	}).CatchAll(func(err error) {
+		output_err = err
+	})
+	return output_err
+} // end CloseUsbmuxClient()
 
 func UsbmuxListen(cbOnData UsbmuxListenCallback, bExitOnCtrlC bool) context.CancelFunc {
 	sigTerm := make(chan os.Signal, 1)
@@ -32,32 +48,34 @@ func UsbmuxListen(cbOnData UsbmuxListenCallback, bExitOnCtrlC bool) context.Canc
 		backoff.RetryNotify(func() error {
 			if mylatch.Counter() <= 0 {
 				return nil
-			}
-			logger.Debug("Connecting to usbmux...")
-			usbMuxClient, err := giDevice.NewUsbmux()
-			if err != nil {
-				return err
-			}
+			} // end if
 			for {
 				if mylatch.Counter() <= 0 {
 					return nil
-				}
-				time.Sleep(5 * time.Second)
+				} // end if
+				time.Sleep(15 * time.Second)
+				logger.Debug("Connecting to usbmux...")
+				usbMuxClient, err := giDevice.NewUsbmux()
+				if err != nil {
+					return err
+				} // end if
 				if _, errBuid := usbMuxClient.ReadBUID(); errBuid != nil {
 					return errBuid
-				}
+				} // end if
+				CloseUsbmuxClient(usbMuxClient)
 				logger.Debug("usbmux health check success")
+				healthCheck <- true
 				if !bIsOk { // transition from not OK to OK
 					logger.Trace("Reset health check backoff algorithm")
 					backoffAlgorithm.Reset()
 					bIsOk = true
-				}
-			}
+				} // end if
+			} // end for
 		}, backoffAlgorithm, func(err error, d time.Duration) {
 			bIsOk = false
-			logger.Warnf("usbmux health check error: %+v", err)
+			logger.Warn(err)
 			healthCheck <- false
-			logger.Debugf("next retry health check in %s", d.String())
+			logger.Debugf("Next retry health check in %s", d.String())
 		})
 		logger.Trace("end health check")
 	})()
@@ -67,11 +85,11 @@ func UsbmuxListen(cbOnData UsbmuxListenCallback, bExitOnCtrlC bool) context.Canc
 			logger.Debugf("Stop listening by signal")
 			if funcStop != nil && *funcStop != nil {
 				(*funcStop)()
-			}
+			} // end if
 			if bExitOnCtrlC {
 				os.Exit(128 + int(syscall.SIGTERM)) // https://itsfoss.com/linux-exit-codes/#code-143-or-sigterm
-			}
-		}
+			} // end if
+		} // end for
 	})(&funcCancelListen)
 	go (func(funcStop *context.CancelFunc) {
 		logger := GetGoRoutineLogger("usbmuxListen")
@@ -82,7 +100,7 @@ func UsbmuxListen(cbOnData UsbmuxListenCallback, bExitOnCtrlC bool) context.Canc
 			usbMuxClient, err := giDevice.NewUsbmux()
 			if err != nil {
 				return err
-			}
+			} // end if
 			usbmuxInput := make(chan giDevice.Device)
 			shutDownFun, errListen := usbMuxClient.Listen(usbmuxInput)
 			*funcStop = func() {
@@ -90,37 +108,37 @@ func UsbmuxListen(cbOnData UsbmuxListenCallback, bExitOnCtrlC bool) context.Canc
 				mylatch.Done()
 				if shutDownFun != nil {
 					shutDownFun()
-				}
+				} // end if
 			}
 			if errListen != nil {
 				return errListen
-			}
+			} // end if
 			if !bIsOk { // transition from not OK to OK
 				logger.Trace("Reset usbmux listen backoff algorithm")
 				backoffAlgorithm.Reset()
 				bIsOk = true
-			}
+			} // end if
 			logger.Debugf("Start listening...")
 			numOnlineDevices := 0
 		loopRead:
 			for {
 				select {
-				case bIsUnhealthy := <-healthCheck:
-					if bIsUnhealthy {
+				case bIsHealthy := <-healthCheck:
+					if !bIsHealthy {
 						logger.Info("Cancel listening because usbmuxd is unhealthy")
 						return fmt.Errorf("usbmux listening is cancelled")
-					}
+					} // end if
 				case d, ok := <-usbmuxInput:
 					if !ok { // channel is closed
 						logger.Info("usbmux input channel is closed")
 						if mylatch.Counter() > 0 {
 							return fmt.Errorf("usbmux listening stopped unexpectedly")
-						}
+						} // end if
 						break loopRead
-					}
+					} // end if
 					if d == nil {
 						continue
-					}
+					} // end if
 					deviceByte, _ := json.Marshal(d.Properties())
 					var device entity.Device
 					errDec := json.Unmarshal(deviceByte, &device)
@@ -131,31 +149,33 @@ func UsbmuxListen(cbOnData UsbmuxListenCallback, bExitOnCtrlC bool) context.Canc
 							numOnlineDevices += 1
 						} else {
 							numOnlineDevices -= 1
-						}
+						} // end if
 					} else {
 						ptrEntityDevice = nil
-					}
+					} // end if
 					var _fStop context.CancelFunc
 					if funcStop != nil {
 						_fStop = *funcStop
-					}
+					} // end if
 					if cbOnData != nil {
 						cbOnData(&d, ptrEntityDevice, errDec, _fStop)
-					}
+					} // end if
 					logger.Debugf("Number of online devices= %d", numOnlineDevices)
 					if numOnlineDevices <= 0 {
 						logger.Info("No devices are online")
-					}
-				}
-			}
+					} // end if
+				} // end select
+			} // end for
+			CloseUsbmuxClient(usbMuxClient)
 			return nil
 		}, backoffAlgorithm, func(err error, d time.Duration) {
 			bIsOk = false
-			logger.Warnf("usbmux listening error: %+v", err)
+			logger.Warn(err)
+			<-healthCheck // clear queued signal
 			logger.Debugf("Next retry listening in %s", d.String())
 		})
 		mylatch.Done()
 		logger.Trace("end usbmux listen")
 	})(&funcCancelListen)
 	return funcCancelListen
-}
+} // end UsbmuxListen()
